cd66885ce56b994abe26830337ec0f3bf53bf2a4
ajay-mandaviya <ajay.mandaviya03@gmail.com>
2023-05-14 22:20:24 +0530

    add screens

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,144 @@
 import React from "react";
-import { Header, Sidebar } from "../../components";
-
+import { Header, Sidebar, Table, UserModal } from "../../components";
+import "./home.css";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import Avatar from "@atlaskit/avatar";
+import Button from "@atlaskit/button";
+import { setModal, setModalUser } from "../../store";
+import { createKey } from "../../helper";
 const Home = () => {
+  const dispatch = useAppDispatch();
+  const { users, loading, selectedUser, modalVisible } = useAppSelector(
+    (state) => state.users
+  );
+
+  const {
+    ageSort,
+    gender,
+    searchByUnivercity,
+    selectedGloodGroup,
+    searchUser,
+  } = useAppSelector((state) => state.filters);
+
+  console.log("searchUser", searchUser);
+
+  const filterUsers = () => {
+    let userWithFilter = users;
+    // if (ageSort) {
+    // const sortedData = [...userWithFilter];
+    // sortedData.sort((a, b) => {
+    //   if (ageSort === "asc") {
+    //     return a.age - b.age;
+    //   } else {
+    //     return b.age - a.age;
+    //   }
+    // });
+    // return (userWithFilter = sortedData);
+    // }
+    if (selectedGloodGroup.length) {
+      userWithFilter = userWithFilter.filter((user) =>
+        selectedGloodGroup.includes(user.bloodGroup)
+      );
+    }
+    if (gender) {
+      userWithFilter = userWithFilter.filter((user) => user.gender === gender);
+    }
+    if (searchByUnivercity) {
+      userWithFilter = userWithFilter.filter((user) =>
+        user.university
+          .toLocaleLowerCase()
+          .includes(searchByUnivercity.toLocaleLowerCase())
+      );
+    }
+    if (searchUser) {
+      userWithFilter = userWithFilter.filter((user) => {
+        const userFullname = `${user?.firstName || ""} ${
+          user?.lastName || ""
+        }`.toLocaleLowerCase();
+        console.log("userFullname", userFullname);
+
+        const userName = user?.username.toLocaleLowerCase() || "";
+        return (
+          userFullname.includes(searchUser.toLocaleLowerCase()) ||
+          userName.includes(searchUser.toLocaleLowerCase())
+        );
+      });
+    }
+    return userWithFilter;
+  };
+
+  const usersRow = filterUsers().map((user) => ({
+    key: `${user.id}`,
+    cells: [
+      {
+        key: createKey(user.image),
+        content: (
+          <Avatar
+            appearance="circle"
+            src={user.image}
+            size="large"
+            name={user.firstName || ""}
+          />
+        ),
+      },
+      {
+        key: createKey(user.firstName),
+        content: user.firstName,
+      },
+      {
+        key: createKey(user.lastName),
+        content: user.lastName,
+      },
+
+      {
+        key: user.email,
+        content: user.email,
+      },
+      {
+        key: user.company?.name,
+        content: user.company?.name,
+      },
+      {
+        key: user.bloodGroup,
+        content: `${user.bloodGroup} `,
+      },
+      {
+        key: user.phone,
+        content: user.age,
+      },
+      {
+        key: "More",
+        content: (
+          <Button
+            onClick={() => {
+              dispatch(setModalUser(user));
+              dispatch(setModal(true));
+            }}
+          >
+            More
+          </Button>
+        ),
+      },
+    ],
+  }));
+
   return (
     <>
       <Header />
-      <div>
-        <Sidebar />
+      <div className="home-container">
+        <div>
+          <Sidebar />
+        </div>
+        <div>
+          <Table
+            rows={usersRow}
+            emptyView={loading ? <h1></h1> : <h1>No users Found</h1>}
+            isRankable
+            isLoading={loading}
+          />
+        </div>
       </div>
+      <UserModal visible={modalVisible} />
     </>
   );
 };
--- src/pages/ShortList/ShortList.tsx
+++ src/pages/ShortList/ShortList.tsx
@@ -1,11 +1,78 @@
 import React from "react";
-import { Header } from "../../components";
+import { Header, Table, UserModal } from "../../components";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import Avatar from "@atlaskit/avatar";
 
+import { createKey } from "../../helper";
+import { setModal, setModalUser } from "../../store";
+import Button from "@atlaskit/button";
 const ShortList = () => {
+  const { shortListUsers } = useAppSelector((state) => state.shortList);
+  const dispatch = useAppDispatch();
+  const { modalVisible } = useAppSelector((state) => state.users);
+  const shortListRow = shortListUsers.map((user: any) => ({
+    key: `${user.id}`,
+    cells: [
+      {
+        key: "Profile",
+        content: (
+          <Avatar
+            appearance="circle"
+            src={user.image}
+            size="large"
+            name={user.firstName || ""}
+          />
+        ),
+      },
+      {
+        key: createKey(user?.firstName),
+        content: user.firstName,
+      },
+      {
+        key: createKey(user?.lastName),
+        content: user.lastName,
+      },
+
+      {
+        key: user.email,
+        content: user.email,
+      },
+      {
+        key: user.company?.name,
+        content: user.company?.name,
+      },
+      {
+        key: user.bloodGroup,
+        content: user.bloodGroup,
+      },
+      {
+        key: user.phone,
+        content: user.phone,
+      },
+      {
+        key: "More",
+        content: (
+          <Button
+            onClick={() => {
+              dispatch(setModalUser(user));
+              dispatch(setModal(true));
+            }}
+          >
+            More
+          </Button>
+        ),
+      },
+    ],
+  }));
+
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div style={{ width: "90vw", margin: "auto", marginTop: "2rem" }}>
+        <Table rows={shortListRow} />
+      </div>
+      <UserModal visible={modalVisible}  withFooter = {false} />
+    </>
   );
 };
 
